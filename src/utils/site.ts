89c1034b742365ffb9155fee90785dbcc9d87ee7
kuloteyesda <kuloteyesda@gmail.com>
2025-07-15 00:31:30 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Pesta Sex';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Gb Sma Cindo Tante China Terbaru Viral';
-export const url = 'https://bokeppestasex.pages.dev';
-export const terbit = '2025-06-27T00:01:03Z';
